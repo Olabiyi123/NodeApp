a393e65d726b945aff9176a83a89dc85cb57a16e
Oladipo Olabiyi <ddolabiy@uwaterloo.ca>
2024-04-26 12:19:00 -0400

    included swagger url

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const options = {
 }
 
 const swaggerSpec = swaggerJSDoc(options);
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 
